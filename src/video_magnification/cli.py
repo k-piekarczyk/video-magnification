da2b4a0dabd76044f762f4db98a47848f692d480
Krzysztof Piekarczyk <krzysztof@piekarczyk.eu>
2022-06-19 21:26:54 +0200

    add laplacian pyramide

--- src/video_magnification/cli.py
+++ src/video_magnification/cli.py
@@ -1,14 +1,17 @@
+from typing import Any, Tuple
 import cv2
 import sys
+import numpy.typing as npt
+import numpy as np
 
 from video_magnification.processing import process_frame_buffer
-from video_magnification.utils.video import VideoFileReader, scale_frame_up
+from video_magnification.utils.video import VideoFileReader, scale_frame_up, laplace_pyramid_step
 
 SCALE_FACTOR = 3
-MAGNIFICATION_FACTOR = 1
+MAGNIFICATION_FACTOR = 4
 
 
-def main():
+def processed_run():
     filepath = sys.argv[1]
 
     vf_reader = VideoFileReader(filepath=filepath)
@@ -22,17 +25,47 @@ def main():
     while cap.isOpened():
         ret, frame = cap.read()
         if ret:
-
-            scaled_up = scale_frame_up(frame=processed_frame_buffer[frame_count], scaling_factor=SCALE_FACTOR)
+            scaled_up = scale_frame_up(frame=processed_frame_buffer[frame_count], scaling_factor=SCALE_FACTOR)[:-6,:,:].astype("uint8")
 
             cv2.imshow("Original", frame)
             cv2.imshow("Redone", scaled_up)
+
+            alpha = .95
+            beta = 1 - alpha
+
+            cv2.imshow("Joined", cv2.addWeighted(frame, alpha, scaled_up, beta, 0.0))
 
             frame_count = frame_count + 1
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         else:
             break
+
+def laplace():
+    filepath = sys.argv[1]
+
+    vf_reader = VideoFileReader(filepath=filepath)
+
+    cap = VideoFileReader(filepath=filepath).get_cap()
+
+    _, frame = cap.read()
+    frame: npt.NDArray[np.uint8]  # just here for type annotation
+    
+    scaled, diff = laplace_pyramid_step(frame=frame)
+
+    cv2.imshow("Original", frame)
+    cv2.imshow("Scaled", scaled)
+    cv2.imshow("Diff", diff)
+
+
+    cv2.waitKey(0)
+    
+
+
+
+def main():
+    # processed_run()
+    laplace()
 
 
 if __name__ == "__main__":
--- src/video_magnification/utils/video/manipulation.py
+++ src/video_magnification/utils/video/manipulation.py
@@ -1,7 +1,10 @@
 import cv2
+import numpy as np
+import numpy.typing as npt
+from typing import Tuple
 
 
-__all__ = ["scale_frame_down", "scale_frame_up"]
+__all__ = ["scale_frame_down", "scale_frame_up", "laplace_pyramid_step"]
 
 
 def scale_frame_down(frame, scaling_factor: int):
@@ -22,3 +25,19 @@ def scale_frame_up(frame, scaling_factor: int):
         frame = cv2.pyrUp(frame)
 
     return frame
+
+def laplace_pyramid_step(frame: npt.NDArray[np.uint8]) -> Tuple[npt.NDArray[np.uint8], npt.NDArray[np.uint8]]:
+    """
+    Scales the given frame down and returns the scaleddown frame and it's diff from original
+    """
+
+    scaled_down = cv2.pyrDown(frame)
+    expanded = cv2.pyrUp(scaled_down)
+
+    diff = cv2.subtract(frame, expanded)
+
+    return (scaled_down, diff)
+    
+
+
+
